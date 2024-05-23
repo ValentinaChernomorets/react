1bef71a5275516b7a71ffb9cd93607d26374fc32
Valentina Chernomorets <vcapustina8@gmail.com>
2024-05-23 11:23:20 +0300

    fixed no keys error

--- client/src/ui/Menu.js
+++ client/src/ui/Menu.js
@@ -7,12 +7,12 @@ const Menu = ({items, sortAsc}) => {
                 .sort(
                     (item1, item2) => (sortAsc ? 1: -1) * (item1.price - item2.price)
                 )
-                .map(item =>
-                <li>
-                    <h2>{item.name}</h2>
-                    <img src={item.image} width="100" alt="images"/>
-                    <p>{item.price.amount}{item.price.currency}</p>
-                </li>
+                .map((item, index) =>
+                    <li key={index}>
+                        <h2>{item.name}</h2>
+                        <img src={item.image} width="100" alt="images"/>
+                        <p>{item.price.amount}{item.price.currency}</p>
+                    </li>
             )}
             </ul>
         </>
